aed81e59713f547ee4878d256380b7527fbf1877
--unset <jeraldthatman@gmail.com>
2024-06-16 00:39:33 -0400

    Update EM page with candle chart and band slider

--- src/daily_candles.js
+++ src/daily_candles.js
@@ -1,8 +1,12 @@
 
 // Format Date Function
 var formatDate = d3.timeFormat("%A %B %d, %Y");
+var formatUTC = d3.utcFormat("%Y-%m-%d");
+var quidkDate = d3.timeFormat("%m/%d");
+var band_slider = d3.select("#band-slider").append("input").attr("type", "range").attr("min", 1).attr("max", 50).attr("value", 20);
 
-function daily_candle_chart(data, title){
+
+function daily_candle_chart(data, title, n=20){
     // set flip to the last close
     const candles = Plot.plot({
         // inset: 10,
@@ -12,17 +16,15 @@ function daily_candle_chart(data, title){
         marginRight: 25,
         x: {
             x: "Date",
-            // type:"time",
-            // domain: d3.extent(data, d => d.Date).filter((d) => d.getUTCDay() > 0 && d.getUTCDay() < 6),
-            // tickFormat: (d, i) => i % 5 === 0 ? formatDate(d) : "",
-            // label: null,
-            // domain: d3.utcDays(...d3.extent(data, (d) => d.Date)).filter((d) => d.getUTCDay() > 0 && d.getUTCDay() < 6), 
+            // domain: d3.utcDays(...d3.extent(data, (d) => d.Date)).filter((d) => d.getUTCDay() > 0 && d.getUTCDay() < 6),
+          // Monday ticks
+          ticks: d3.utcMondays(...d3.extent(data, (d) => d.Date)),
         },
         y: {
             label: title + " Price",
             grid:true,
             padding: 0.5,
-            domain: [d3.min(data, d => d.Low) - 1, d3.max(data, d => d.High)+ 0.5],
+            // domain: [d3.min(data, d => d.Low) - 1, d3.max(data, d => d.High)+ 2],
         },
         color: {domain: [-1, 0, 1], range: ["#e41a1c", "currentColor", "#4daf4a"]},
         marks: [
@@ -45,7 +47,11 @@ function daily_candle_chart(data, title){
             // Plot.crosshair(data, {x: "Date", y: "Close", textFill:'black'}),
             Plot.dot(data, Plot.pointerX({x: "Date", y: "Close", stroke: "yellow"})),
             Plot.ruleY(data, Plot.pointerY({px: "Date", y: "Close", stroke: "red"})),
-            Plot.text(data, Plot.pointerX({px: "Date", py: "Close", dy: -17, frameAnchor: "top-right", fontVariant: "tabular-nums", text: (d) => [`Date: ${formatDate(d.Date)}`, `Close: \$${d.Close.toFixed(2)}`].join("   ")}))
+            Plot.text(data, Plot.pointerX({px: "Date", py: "Close", dy: -17, frameAnchor: "top-right", fontVariant: "tabular-nums", text: (d) => [`Date: ${formatDate(d.Date)}`, `Close: \$${d.Close.toFixed(2)}`].join("   ")})),
+            // Plot.lineY(data, Plot.windowY({k: 10, fn: d3.mean, stroke: "black"}, {x: "Date", y: "Close"})),
+            Plot.lineY(data, Plot.mapY(Plot.bollinger({n: n, k: -2}), {x: "Date", y: "Close", stroke: "red"})),
+            Plot.lineY(data, Plot.mapY(Plot.bollinger({n: n, k: 0}), {x: "Date", y: "Close", stroke: "white"})),
+            Plot.lineY(data, Plot.mapY(Plot.bollinger({n: n, k: 2}), {x: "Date", y: "Close", stroke: "green"})),
 
           ]
     
@@ -53,6 +59,37 @@ function daily_candle_chart(data, title){
 
     return candles
 }
+
+var sidePanel = d3.select("#text-box-em").append("div");
+function em_text(stock){
+    d3.json("../data/contracts/em_ext.json").then(function(data) {
+        data.forEach(function(d) {
+            d.stock = d.stock;
+            d.expiry = new Date(d.expiry);
+            d.last_close = +d.stk_price;
+            d.em = +d.em;
+            d.pct_change = +d['%'];
+            return d;
+            }); // forEach Loop 
+
+            var filteredData = data.filter(d => d.stock === stock);
+            var sidePanelData = filteredData.map(d => d);
+            sidePanel.html("");
+            sidePanel.append("p").text(
+                "$" + stock.toUpperCase() + " " +
+                "Pricing in ± $" + sidePanelData[0].em + 
+                " ( ±" + (100 * round(sidePanelData[0].pct_change)) + "%)" + 
+                " by " + quidkDate(sidePanelData[0].expiry)
+            ).attr("class", "card-text");
+            sidePanel.append("p").text("Last Close: $" + sidePanelData[0].last_close);
+
+    }).catch(function(error) {
+        console.log(error);
+    });
+
+};
+
+
 
 candle_selector = d3.select("#candle-selection").append("select");
 
@@ -62,7 +99,7 @@ d3.json('../data/prices/ohlcv.json').then(function(data){
         candle_selector.append("option").text("$" + stock.toUpperCase()).property("value", stock);
     })
 
-    function updateCandle(stock){
+    function updateCandle(stock, n = 10){
         d3.select("#candles-chart").selectAll("*").remove();
         var candle_data = data[stock];
             candle_data.forEach(function(d){
@@ -74,10 +111,12 @@ d3.json('../data/prices/ohlcv.json').then(function(data){
             d.Volume = +d.Volume;
             return d;
         });
-        console.log(candle_data);
+        // console.log(candle_data);
         var title = stock.toUpperCase();
         var candle_chart = daily_candle_chart(candle_data, title);
         d3.select("#candles-chart").append(() => candle_chart);
+        sidePanel.selectAll("*").remove();
+        em_text(stock);
 
     }
     
@@ -87,6 +126,13 @@ d3.json('../data/prices/ohlcv.json').then(function(data){
     candle_selector.on("change", function(){
         var stock = d3.select(this).property("value");
         updateCandle(stock);
+    });
+
+    // update the bollinger band
+    band_slider.on("input", function(){
+        var stock = candle_selector.property("value");
+        var n = this.value;
+        updateCandle(stock, n);
     });
 
     // initial plot
--- src/em_plot.js
+++ src/em_plot.js
@@ -2,7 +2,7 @@
 // Create a plot that shows the current close and the projected close for the next expiration dates based on the dates in the em_ext.json file
 
 // Create a Dropbox for each stock in the data
-var stockDrop = d3.select("#stock-selection").append("select");
+var stockDrop = d3.select("#candle-selection");
 
 // Create div for the side panel body 
 var sidePanel = d3.select("#text-box-em").append("div");
@@ -28,123 +28,7 @@ d3.csv("../data/prices/close.csv").then(function(price_data) {
             d.pct_change = +d['%'];
             return d;
             // append the stock to the dropdown
-
             }); // forEach Loop 
-            // append the stock to the dropdown
-            var stocks = data.map(d => d.stock);
-            var uniqueStocks = [...new Set(stocks)];
-            // sort uniqueStocks
-            uniqueStocks.sort();
-            uniqueStocks.forEach(function(stock) {
-                stockDrop.append("option").text("$"+stock.toUpperCase()).property("value", stock);
-            }); // forEach Loop
-
-            // Create a function to update the plot
-            function updatePlot(stock) {
-                var closeData = price_data.filter(d => d[stock.toUpperCase()]);
-                var filteredData = data.filter(d => d.stock === stock);
-                var last_close = closeData[closeData.length - 1][stock.toUpperCase()];
-                var last_close_up = Number(last_close) + 0;
-                var last_close_down = Number(last_close) - 0;
-                // console.log(last_close);
-
-                var trace1 = {
-                    // x: filteredData.map(d => d.expiry),
-                    // y: filteredData.map(d => d.last_close),
-                    x: closeData.map(d => new Date(d.Date)),
-                    y: closeData.map(d => +d[stock.toUpperCase()]),
-                    mode: 'lines',
-                    name: 'Close',
-                    line: {
-                        color: 'white',
-                        width: 1
-                    }
-                };
-                // Upper Line Chart 
-                var trace2 = {
-                    // x: filteredData.map(d => d.expiry),
-                    // y: filteredData.map(d => d.em + d.last_close),
-                    //  Add the last data point from closeData to the begining of the filteredData
-                    x: [closeData[closeData.length - 1].Date, ...filteredData.map(d => d.expiry)],
-                    y: [last_close_up, ...filteredData.map(d => d.em + d.last_close)],
-                    mode: 'lines+markers',
-                    name: 'Upper Expectations',
-                    line: {
-                        color: 'green',
-                        width: 1.5
-                    }
-                };
-                // Lower Line Chart
-                var trace3 = {
-                    // x: filteredData.map(d => d.expiry),
-                    // y: filteredData.map(d => -d.em + d.last_close),
-                    x: [closeData[closeData.length - 1].Date, ...filteredData.map(d => d.expiry)],
-                    y: [last_close_down, ...filteredData.map(d => -d.em + d.last_close)],
-                    mode: 'lines+markers',
-                    name: 'Lower Expectations',
-                    line: {
-                        color: 'red',
-                        width: 1.5
-                    }
-                };
-                var trace4 = {
-                    x: filteredData.map(d => d.expiry),
-                    y: filteredData.map(d => -d.em + d.last_close),
-                    fill: 'tonexty',
-                    fillcolor: "rgba(68, 68, 70, 0.9)", 
-                    line: {width: 0}
-                };
-                
-                var gauge_indicator = [
-                    {
-                      type: "indicator",
-                      mode: "number+gauge+delta",
-                      gauge: { shape: "bullet" },
-                      delta: { reference: 300 },
-                      value: 220,
-                      domain: { x: [0, 1], y: [0, 1] },
-                      title: { text: "Profit" }
-                    }
-                  ];
-
-                // var traces = [trace1, trace2, trace4, trace3];
-                var traces = [trace1, trace2,  trace3];
-                var layout = {
-                    title: '',
-                    xaxis: {
-                        title: 'Date',
-                        showgrid: true,
-                        zeroline: false,
-                        showline: true,
-                        tickfont: {color: 'white', size: 8}, 
-                        titlefont: {color: 'white', size: 10},
-                        gridcolor: 'rgba(255, 255, 255, 0.1)',
-                    },
-                    yaxis: {
-                        title: 'Close',
-                        showline: true, 
-                        showgrid: true,
-                        tickfont: {color: 'white', size: 8},
-                        titlefont: {color: 'white', size: 10},
-                        gridcolor: 'rgba(255, 255, 255, 0.1)',
-                    },
-                    // Horizontal Legend above the plot
-                    legend: {
-                        x: 0,
-                        y: 1.12,
-                        bgcolor: 'rgba(255, 255, 255, 0)',
-                        bordercolor: 'rgba(255, 255, 255, 0)',
-                        orientation: 'h'
-                    }, 
-                    showlegend: false,
-                    paper_bgcolor: 'rgba(0,0,0,0)',
-                    plot_bgcolor: 'rgba(0,0,0,0)',
-                    
-
-                    margin: {l:50,r: 50,b: 90,t: 50,pad: 1}, 
-                };
-                Plotly.newPlot('plot_area', traces, layout, {staticPlot: false, responsive: true, displayModeBar: false});
-            } // updatePlot function
 
             // Create a function to update the side panel
             function updateSidePanel(stock) {
@@ -152,30 +36,25 @@ d3.csv("../data/prices/close.csv").then(function(price_data) {
                 var sidePanelData = filteredData.map(d => d);
                 sidePanel.html("");
                 sidePanel.append("p").text(
-                    "$" + stock.toUpperCase() + " " +
+                    "$" + stock + " " +
                     "Pricing in ± $" + sidePanelData[0].em + 
                     " ( ±" + (100 * round(sidePanelData[0].pct_change)) + "%)" + 
                     " by " + formatDate(sidePanelData[0].expiry)
                 ).attr("class", "card-text");
                 sidePanel.append("p").text("Last Close: $" + sidePanelData[0].last_close);
-            } // updateSidePanel function
+            }; // updateSidePanel function
 
             // Create a function to handle the change event
             function optionChanged(stock) {
-                updatePlot(stock);
                 updateSidePanel(stock);
-            } // optionChanged function
+            }; // optionChanged function
 
-            // Call the updatePlot function
-            updatePlot(uniqueStocks[0]);
-
-            // Call the updateSidePanel function
-            updateSidePanel(uniqueStocks[0]);
 
             // Call the optionChanged function
-            stockDrop.on("change", function() {
+            d3.select("candle-selector").on("change", function() {
                 var stock = d3.select(this).property("value");
-                optionChanged(stock);
+                console.log(stock)
+                // optionChanged(stock);
             }); // on change event
 
 
